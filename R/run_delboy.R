3086f5bacf831d5f8476b474fa008b5a6ea9a07a
Alex Kalinka <Alex.Kalinka@cancer.org.uk>
2020-09-06 22:36:54 +0200

    getting started.

---
+++ R/run_delboy.R
@@ -0,0 +1,7 @@
+#' run_delboy
+#'
+#' Performs a differential-representation analysis using an elastic-net logistic regression approach.
+#'
+#' @param data A data frame containing count data for two different groups and their replicates. Can be a path to a file. If the data needs to be normalized, indicate this using the `normalize` argument.
+#' @param normalize
+#'
