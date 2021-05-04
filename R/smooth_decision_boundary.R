155d5e5b6cc15bbd39f9b1ed0f03a3969fe8b17d
Alex Kalinka <Alex.Kalinka@cancer.org.uk>
2021-05-04 22:18:27 +0100

    smooth low-end DB.

--- R/smooth_decision_boundary.R
+++ R/smooth_decision_boundary.R
@@ -1,3 +1,27 @@
+# Helper functions.
+
+# Anti-tonic, convex enforcing.
+.anti_tonic_convex <- function(data, y){
+  # data arranged by x (increasing).
+  runn_delta <- 0
+  for(i in 1:nrow(data)){
+    if(i == 1) next
+    runn_delta <- data[i,y] - data[i-1,y]
+    # Anti-tonic.
+    if(data[i,y] > data[i-1,y]){
+      data[i,y] <- data[i-1,y]
+    }else{
+      if(i == 2) next
+      # Convex.
+      if(data[i,y] - data[i-1,y] < runn_delta){
+        data[i,y] <- data[i-1,y] - runn_delta
+      }
+    }
+  }
+  return(data)
+}
+
+
 #' smooth_decision_boundary
 #' 
 #' Smoothes the fold-change expression decision boundary such that it is a convex, monotonically decreasing function of expression level.
@@ -19,22 +43,10 @@ smooth_decision_boundary <- function(db, entry_point = 1.25){
       dplyr::arrange(log10_baseExpr)
     
     # 1. High-end values.
-    runn_delta <- 0
-    for(i in 1:nrow(db_high)){
-      if(i == 1) next
-      runn_delta <- db_high$abs_log2FoldChange[i] - db_high$abs_log2FoldChange[i-1]
-      # Anti-tonic.
-      if(db_high$abs_log2FoldChange[i] > db_high$abs_log2FoldChange[i-1]){
-        db_high$abs_log2FoldChange[i] <- db_high$abs_log2FoldChange[i-1]
-      }else{
-        if(i == 2) next
-        # Convex.
-        if(db_high$abs_log2FoldChange[i] - db_high$abs_log2FoldChange[i-1] < runn_delta){
-          db_high$abs_log2FoldChange[i] <- db_high$abs_log2FoldChange[i-1] - runn_delta
-        }
-      }
-    }
+    db_high <- .anti_tonic_convex(db_high, "abs_log2FoldChange")
+    
     db$abs_log2FoldChange[match(db_high$log10_baseExpr, db$log10_baseExpr)] <- db_high$abs_log2FoldChange
+    
     # Make sure DB extends across range of interest.
     if(max(db$log10_baseExpr, na.rm=T) < 6){
       db<- rbind(db, data.frame(log10_baseExpr = 6,
@@ -48,9 +60,15 @@ smooth_decision_boundary <- function(db, entry_point = 1.25){
         stop(paste("IQR of abs_log2FoldChange:",iqr_fc))
       spl_fit <- stats::smooth.spline(db_low$abs_log2FoldChange, db_low$log10_baseExpr)
       db_low_expr <- stats::predict(spl_fit, seq(max(db_low$abs_log2FoldChange),4,0.02))
+      db_low <- rbind(db_low,
+                      data.frame(log10_baseExpr = db_low_expr$y, 
+                           abs_log2FoldChange = db_low_expr$x)) %>%
+        dplyr::arrange(abs_log2FoldChange)
+
+      db_low <- .anti_tonic_convex(db_low, "log10_baseExpr")
+      
       db %<>%
-        rbind(data.frame(log10_baseExpr = db_low_expr$y, 
-                         abs_log2FoldChange = db_low_expr$x))
+        rbind(db_low)
     }
     
     return(db)
