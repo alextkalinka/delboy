ea1d14e55e7ccbbba9241b0ae3a1d6eedc396ee2
Alex Kalinka <Alex.Kalinka@cancer.org.uk>
2021-05-07 16:32:40 +0100

    use all combinations of treat samps for validation; max.iter arg for run_delboy.

--- R/evaluate_performance_rnaseq_calls.R
+++ R/evaluate_performance_rnaseq_calls.R
@@ -6,6 +6,7 @@
 #' @param group_1 A character vector naming the columns that belong to group 1.
 #' @param group_2 A character vector naming the columns that belong to group 2.
 #' @param gene_column A character string naming the column containing gene names.
+#' @param max.iter An integer value indicating the maximum number of validation samples.
 #' @param num_non_null An integer value indicating the number of genes to add signal to.
 #' @param lfc A vector of logFC values for non-null cases.
 #' @param lfc_dens A vector of density estimates for the logFC values given in `lfc`.
@@ -16,69 +17,78 @@
 #' @importFrom seqgendiff thin_diff
 #' @importFrom dplyr select
 #' @importFrom rlang sym !!
-evaluate_performance_rnaseq_calls <- function(data, group_1, group_2, gene_column,
+evaluate_performance_rnaseq_calls <- function(data, group_1, group_2, gene_column, max.iter,
                                               num_non_null, lfc, lfc_dens, alpha){
   tryCatch({
     # 1. Prep for seqgendiff.
     data.m <- delboy::prep_count_matrix(data, group_1, group_2, gene_column)
 
+    # 2. All combinations of treatment samples (preserving the number of treatment samples in original data [length(group_2)]).
+    all_treat_comb <- delboy::all_combinations_treat_samples(c(group_1, group_2), length(group_2))
+    if(is.null(max.iter)){
+      num_treat_comb <- ncol(all_treat_comb)
+    }else{
+      all_treat_comb <- all_treat_comb[,1:max.iter]
+    }
+    
     all_val_hits <- NULL
     all_val_perf <- NULL
     # Multiple val samples to improve SVM estimate.
-    for(i in 1:3){
-      # 2. Sample logFC values for num_non_null cases.
+    for(i in 1:ncol(all_treat_comb)){
+      # 3. Sample logFC values for num_non_null cases.
       lfc_samp <- sample(lfc, num_non_null, prob = lfc_dens/sum(lfc_dens), replace = T)
 
-      # 3. Sample genes to add signal to.
+      # 4. Sample genes to add signal to.
       genes_signal <- sample(data[,gene_column], num_non_null, replace = F)
       names(lfc_samp) <- genes_signal
 
-      # 4. Create coefficient matrix for seqgendiff.
+      # 5. Create coefficient matrix for seqgendiff.
       coef_mat <- delboy::make_coef_matrix(data, lfc_samp, gene_column)
 
-      # 5. Create design matrix for seqgendiff.
-      design_mat <- delboy::make_design_matrix(group_1, group_2)
+      # 6. Create design matrix for seqgendiff.
+      treat_samps <- all_treat_comb[,i]
+      design_mat <- delboy::make_design_matrix(group_1, group_2, treat_samps)
 
-      # 6. Add signal using seqgendiff's binomial-thinning approach.
+      # 7. Add signal using seqgendiff's binomial-thinning approach.
       thout <- seqgendiff::thin_diff(mat = data.m,
                                      design_fixed = design_mat,
                                      coef_fixed = coef_mat)
 
-      # 7. Prep bthin matrix for use in DiffExp analyses.
+      # 8. Prep bthin matrix for use in DiffExp analyses.
       data.bthin <- delboy::prep_bthin_matrix_diffrep(data, thout$mat,
                                                       colnames(data.m),
                                                       as.logical(c(design_mat)),
                                                       gene_column)
 
-      # 8. Run DESeq2 on bthin data.
+      # 9. Run DESeq2 on bthin data.
       group_1.v <- colnames(data.bthin %>%
                               dplyr::select(- !!rlang::sym(gene_column)))[!as.logical(c(design_mat))]
       group_2.v <- colnames(data.bthin %>%
                               dplyr::select(- !!rlang::sym(gene_column)))[as.logical(c(design_mat))]
       deseq2_res <- delboy::run_deseq2(data.bthin, group_1.v, group_2.v, gene_column)
 
-      # 9. Prep data for Elastic-net logistic regression.
+      # 10. Prep data for Elastic-net logistic regression.
       data.elnet <- delboy::prep_elnet_data(data.bthin, group_1.v, group_2.v, gene_column)
 
-      # 10. Run Elastic-net logistic regression on bthin data.
+      # 11. Run Elastic-net logistic regression on bthin data.
       elnet.lr <- delboy::run_elnet_logistic_reg(as.matrix(data.elnet[,3:ncol(data.elnet)]),
                                                factor(data.elnet$treat),
                                                alpha = alpha)
 
-      # 11. Extract performance statistics.
+      # 12. Extract performance statistics.
       perf_stats <- delboy::perf_stats_rnaseq(elnet.lr, deseq2_res, lfc_samp)
 
-      # 12. Collate TP, FN, and FP into a data frame to aid comparisons.
+      # 13. Collate TP, FN, and FP into a data frame to aid comparisons.
       delboy_hit_df <- delboy::make_delboy_hit_comparison_table(elnet.lr,
                                                                 deseq2_res,
                                                                 lfc_samp)
       all_val_hits <- rbind(all_val_hits, delboy_hit_df)
       all_val_perf <- rbind(all_val_perf, perf_stats)
     }
-    # 13. Performance stats.
+    # 14. Performance stats.
     pstats_summ <- delboy::calculate_perf_stats(all_val_perf)
     
-    # 14. Is the validation data sufficient for finding SVM decision boundary?
+    # 15. Is the validation data sufficient for finding SVM decision boundary?
     if(sum(all_val_hits$hit_type == "True_Positive") == 0)
       stop("* no true positives in validation data: unable to validate algorithms *")
     
@@ -89,10 +99,10 @@ evaluate_performance_rnaseq_calls <- function(data, group_1, group_2, gene_colum
       use_fn <- FALSE
     }
  
-    # 15. SVM for false positive classification.
+    # 16. SVM for false positive classification.
     svm_validation <- delboy::svm_false_positive_classification(all_val_hits, use_fn = use_fn)
 
-    # 16. Build return object of class 'delboy_performance'.
+    # 17. Build return object of class 'delboy_performance'.
     ret <- list(lfc_samp = lfc_samp,
                 data.bthin = data.bthin,
                 elnet_lr_res = elnet.lr,
--- R/run_delboy.R
+++ R/run_delboy.R
@@ -8,6 +8,7 @@
 #' @param filter_cutoff A numerical value indicating the cutoff below which (summed across all replicates) a gene will be removed from the data. For example, to keep only genes with more than 1 TPM on average across both groups, set the cutoff to 10 if there are 10 replicates in total.
 #' @param gene_column A character string naming the column containing gene names.
 #' @param batches A named character vector identifying the batch structure with names identifying sample columns in the data input. The length must equal `length(group_1) + length(group_2)`. If `NULL`, there are no batches, or batches have already been corrected. Defaults to `NULL`. Batch correction will be conducted using `sva::ComBat` using non-parametric priors.
+#' @param max.iter An integer value indicating the maximum number of validation samples. `NULL` (default) indicates all sample combinations will be taken.
 #' @param bcorr_data_validation `NULL` if no batch (signal) corrected data is already available for validation. Otherwise, a data frame of treatment-corrected data should be supplied (to speed up validation, if already available). Defaults to `NULL`. Batch correction will be conducted using `sva::ComBat` using non-parametric priors.
 #' @param alpha The elastic-net regression penalty, between 0 and 1. If `NULL` (default) this is chosen automatically.
 #'
@@ -21,7 +22,8 @@
 #' * Kalinka, A. T. 2020. Improving the sensitivity of differential-expression analyses for under-powered RNA-seq experiments. bioRxiv [10.1101/2020.10.15.340737](https://doi.org/10.1101/2020.10.15.340737).
 #' * Patro, R. et al. 2017. Salmon provides fast and bias-aware quantification of transcript expression. Nature Methods 14: 417-419.
 run_delboy <- function(data, group_1, group_2, filter_cutoff, gene_column,
-                       batches = NULL, bcorr_data_validation = NULL, alpha = NULL){
+                       batches = NULL, max.iter = NULL,
+                       bcorr_data_validation = NULL, alpha = NULL){
   # Random samples taken.
   set.seed(1)
 
@@ -115,6 +117,7 @@ run_delboy <- function(data, group_1, group_2, filter_cutoff, gene_column,
   cat("Performance evaluation to validate results...\n")
   perf_eval <- suppressWarnings(
     delboy::evaluate_performance_rnaseq_calls(data.bc, group_1, group_2, gene_column,
+                                              max.iter,
                                               non.null$num.non_null,
                                               lfdr.lfc$non_null.lfc,
                                               lfdr.lfc$non_null.dens,
